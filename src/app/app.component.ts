804dfe05718c688af68551b1b50a6ae6b83fbb55
bez <Behzad.p.ashrafi@gmail.com>
2023-04-18 23:00:48 +0300

    create api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './service/weather.service';
+import { WeatherData } from './module/weather/weather.module';
+import { Forecast } from './module/forecast/forecast.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private service: WeatherService) { }
+
+  weatherdata?: WeatherData;
+
+  ngOnInit(): void {
+    this.service.getWeatherData('london')
+      .subscribe({
+        next: (response) => {
+          this.weatherdata = response;
+        }
+      }),
+
+      this.service.getforecastData('london')
+        .subscribe({
+          next: (response) => {
+            this.weatherdata = response;
+            console.log(response)
+          }
+
+        })
+  }
   title = 'weatherApp';
 }
---
+++ src/app/service/weather.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { WeatherData } from '../module/weather/weather.module';
+import { Observable} from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  baseUrl = 'http://api.weatherapi.com/v1'
+  apikey = "4724d3e275d4442a956191121231804";
+
+
+
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    const data = `${this.baseUrl}/current.json?key=${this.apikey}`
+    return this.http.get<WeatherData>(data, {
+      params: new HttpParams()
+        .set('q', cityName)
+    })
+  }
+
+  getforecastData(cityName: string): Observable<WeatherData> {
+    const data = `${this.baseUrl}/forecast.json?key=${this.apikey}&days=1`
+    return this.http.get<WeatherData>(data, {
+      params: new HttpParams()
+        .set('q', cityName)
+    })
+  }
+
+
+
+}
+
